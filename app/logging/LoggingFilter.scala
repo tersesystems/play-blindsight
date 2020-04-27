6e330333e1f2dfa26626ab26549ac57700fbb6ce
Will Sargent <will.sargent@gmail.com>
2020-04-27 08:59:35 -0700

    Only show non-asset requests

--- app/logging/LoggingFilter.scala
+++ app/logging/LoggingFilter.scala
@@ -1,5 +1,6 @@
 package logging
 
+import com.tersesystems.blindsight.api.Markers
 import javax.inject.Inject
 import play.api.mvc.{EssentialAction, EssentialFilter}
 
@@ -11,13 +12,17 @@ class LoggingFilter @Inject()(implicit ec: ExecutionContext) extends EssentialFi
     import logging.HoneycombFlowBehavior._
     import HoneycombImplicits._
 
-    next(request).map { result =>
-      // The root span has to be logged _last_, after the child spans.
-      val rootSpan = request.attrs(Attrs.spanInfo)
-      val logger = getLogger(request)
-      logger.info(spanMarkerFactory(rootSpan),
-        s"${rootSpan.name()} exit, duration ${rootSpan.duration()}")
-      result
+    if (request.path.startsWith("/assets")) {
+      next(request)
+    } else {
+      next(request).map { result =>
+        // The root span has to be logged _last_, after the child spans.
+        val rootSpan = request.attrs(Attrs.spanInfo)
+        val logger = getLogger(request)
+        logger.info(spanMarkerFactory(rootSpan),
+          s"${rootSpan.name()} exit, duration ${rootSpan.duration()}")
+        result
+      }
     }
   }
 }
